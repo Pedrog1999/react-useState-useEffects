ce2f032abaca42c432f4edc22df2da854cba757a
Pedrog1999 <pedrogianibelli1999@gmail.com>
2026-04-20 21:52:08 -0300

    local storage

--- lista-tareas/src/components/Contador.jsx
+++ lista-tareas/src/components/Contador.jsx
@@ -1,16 +1,23 @@
 import { useState, useEffect } from 'react'
 
 function Contador() {
-  const [contador, setContador] = useState(0)
 
+  const [contador, setContador] = useState(() => {
+    const guardado = localStorage.getItem('contador')
+    return guardado ? parseInt(guardado) : 0
+  })
+
+ 
   useEffect(() => {
+    localStorage.setItem('contador', contador)
     console.log('contador actualizado:', contador)
   }, [contador])
 
   return (
     <div className="contador">
       <p>Contador: {contador}</p>
-      <button onClick={() => setContador(contador + 1)}>Sumar</button>
+      <button onClick={() => setContador(contador + 1)}>Incrementar</button>
+      <button onClick={() => setContador(contador - 1)}>Decrementar</button>
     </div>
   )
 }
